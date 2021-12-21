d6b8d68e4b732915be81d00fcff061205eba0ad9
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2021-12-21 12:15:41 +0000

    Fixed about window title

--- .qt_for_python/uic/youtubedl_about.py
+++ .qt_for_python/uic/youtubedl_about.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'youtubedl_about.ui'
+# Form implementation generated from reading ui file 'c:\Users\mateus\Documents\python\youtubedl_GUI_pyqt\youtubedl_about.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
