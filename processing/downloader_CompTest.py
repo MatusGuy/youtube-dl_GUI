6977c2f4e02335fa08da3379463687da950af2bd
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2021-12-28 23:27:29 +0000

    Added download component test

---
+++ processing/downloader_CompTest.py
@@ -0,0 +1,54 @@
+import sys,os
+sys.path.insert(1,'.')
+
+from PyQt5.QtWidgets import (QApplication,QMainWindow)
+
+from processing import downloader as dl
+from dist import pydist as pd
+
+class Dl_CompTest(object):
+
+    downloader = dl.Downloader()
+
+    def __init__(self):
+        #super().__init__()
+        self.downloader = dl.Downloader(pd.__PyDist__._WorkDir+".\\youtube-dl\\",self.ConsoleAddLine,self.Downloaded_Ended)
+
+    def ConsoleAddLine(self,text):
+        print (f"New Line:\t{text}")
+
+    def Downloaded_Ended(self,error):
+        print (f"Ended. Error code:\t{error}")
+        self.appwindow.close()
+        
+
+    def main(self,app):
+        print ("Downloader Componente Test Begin")
+        self.appwindow = QMainWindow()
+
+        print ("Preparing Config")
+        Config={
+            "URL":"https://www.youtube.com/watch?v=jNQXAC9IVRw",
+            "AUDIO_ONLY":True,
+            "OUTPUT":".\\processing\\testdw.mp3",
+            "TEMPLATE":"",
+            "RANGE":"1-1",
+        }
+        print (Config)
+        print (f"Start Downloading...")
+
+        self.downloader.StartDownload(Config)
+
+        resp=app.exec_()
+
+        print (f"Donwload done!")  
+
+        print ("Downloader Componente Test End")
+        os.system("del .\\processing\\testdw.mp3")
+        return (resp)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    theApp= Dl_CompTest()
+    sys.exit(theApp.main(app))
+
