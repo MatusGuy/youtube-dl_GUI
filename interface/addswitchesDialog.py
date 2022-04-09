6a18340f4ef79989e8ed8802fc8665bd5c5afb5c
MatusGuy <mateu.axe@gmail.com>
2022-04-09 11:05:01 +0100

    Additional switches dialog functions

--- interface/addswitchesDialog.py
+++ interface/addswitchesDialog.py
@@ -14,6 +14,7 @@ class AdditionalSwitchesDialog(ui):
     dialog = QDialog
 
     resp = str
+    audio = bool
 
     def __init__(self,savedswitches="",windowicon=""):
         self.dialog = QDialog(flags = Qt.WindowType.WindowCloseButtonHint)
@@ -24,6 +25,9 @@ class AdditionalSwitchesDialog(ui):
         self.HelpMenu.setIcon(QIcon(pd.__PyDist__._WorkDir+"assets/help_index.png"))
 
         self.OkButton.pressed.connect(self.Close)
+
+    def _SetAudio(self,audio:bool):
+        self.audio = audio
     
     def Open(self):
         self.dialog.open()
@@ -36,6 +40,39 @@ class AdditionalSwitchesDialog(ui):
         return self.Get()
     
     def Set(self,value:str):
-        return self.TextEdit.setPlainText(value)
-    def Get(self) -> str:
+        """Changes additional switches depending if global, video, or audio is chosen"""
+
+        if self.GlobalOption.isChecked():
+            self.TextEdit.setPlainText(value)
+        else:
+            if self.audio:
+                self.AudioTextEdit.setPlainText(value)
+            else:
+                self.VideoTextEdit.setPlainText(value)
+    def SetGlobal(self,value:str):
+        self.TextEdit.setPlainText(value)
+    def SetVideo(self,value:str):
+        self.VideoTextEdit.setPlainText(value)
+    def SetAudio(self,value:str):
+        self.AudioTextEdit.setPlainText(value)
+    
+    def _Get(self) -> str:
         return self.TextEdit.toPlainText().replace("\r"," ").replace("\n"," ")
+    def Get(self):
+        """Returns additional switches depending if global, video, or audio is chosen"""
+        
+        if self.GlobalOption.isChecked():
+            resp = self.TextEdit.toPlainText()
+        else:
+            if self.audio:
+                resp = self.AudioTextEdit.toPlainText()
+            else:
+                resp = self.VideoTextEdit.toPlainText()
+        
+        return resp
+    def GetGlobal(self):
+        return self.TextEdit.toPlainText()
+    def GetVideo(self):
+        return self.VideoTextEdit.toPlainText()
+    def GetAudio(self):
+        return self.AudioTextEdit.toPlainText()
